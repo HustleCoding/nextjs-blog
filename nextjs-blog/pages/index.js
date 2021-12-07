8da0fb3f22400086097a8011466547291713bbad
Florin Dobinciuc <florindobinciuc@Florins-MBP.Dlink>
2021-12-07 15:48:35 +0200

    [components directory added]

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -53,13 +54,7 @@ export default function Home() {
       </main>
 
       <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by <img src="/vercel.svg" alt="Vercel" className="logo" />
-        </a>
+        <h2>Simple Footer</h2>
       </footer>
 
       <style jsx>{`
